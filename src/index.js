a369a62dc8e45fd9b30bb58b03427c9b43ea4b65
MaksymilianBilski <biger9@o2.pl>
2022-10-15 13:11:47 +0200

    obczajony debounce/wyswietlanie wlasciwosci kraju po wpisaniu nazwy

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+import { add, debounce } from 'lodash';
 import './css/styles.css';
+const debounce = require('lodash.debounce');
+const input = document.querySelector('input[type=text]');
+console.log(input);
 
 const DEBOUNCE_DELAY = 300;
 const DATABASE =
@@ -6,33 +10,51 @@ const DATABASE =
 
 const DATABASE2 = 'https://restcountries.com/v3.1/name/';
 
-fetch(DATABASE)
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    for (let i = 0; i < data.length; i++) {
-      console.log(data[i].name);
-    }
-    console.log(data);
-  })
-  .catch(data => {
-    alert('failed to load the resources!');
-  });
+// fetch(DATABASE)
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(data => {
+//     // for (let i = 0; i < data.length; i++) {
+//     //   console.log(data[i].name);
+//     // }
+//     console.log(data);
+//   })
+//   .catch(data => {
+//     alert('failed to load the resources!');
+//   });
 
-function fetchCountry(name) {
+function fetchCountries(name) {
   fetch(DATABASE2 + name)
     .then(response => {
       return response.json();
     })
     .then(data => {
-      // for (let i = 0; i < data.length; i++) {
-      //   console.log(data[i].name);
-      // }
-      console.log(data);
+      let arrLang = [];
+      for (lang in data[0].languages) {
+        arrLang.push(lang);
+      }
+      console.log(
+        data[0].name.official +
+          ', ' +
+          data[0].capital +
+          ', ' +
+          data[0].population +
+          ', ' +
+          data[0].flags.svg +
+          ', ' +
+          arrLang
+      );
     })
     .catch(data => {
       alert('failed to load the resources!');
     });
 }
-fetchCountry('poland');
+fetchCountries('belgium');
+
+input.addEventListener(
+  'click',
+  debounce(() => {
+    console.log('test debouncea');
+  }, 300)
+);
