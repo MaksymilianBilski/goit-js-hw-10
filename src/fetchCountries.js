e4d0e21fceaadefa14dc7287d3ea1de4ebe98ae7
MaksymilianBilski <biger9@o2.pl>
2022-10-17 17:27:48 +0200

    inna metoda

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,68 +1,77 @@
-const input = document.querySelector('input[type=text]');
-const list = document.querySelector('.country-list');
-const DATABASE2 = 'https://restcountries.com/v3.1/name/';
-import { Notify } from '../node_modules/notiflix/build/notiflix-notify-aio';
+const input = document.querySelector('input#search-box');
+const URL_COUNTRIES_API = 'https://restcountries.com/v3.1/name/';
 
 function fetchCountries(name) {
-  name = input.value.trim();
-  if (list.childNodes.length > 0) {
-    list.innerHTML = '';
-  }
-  fetch(DATABASE2 + name)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      console.log(data.status);
-      console.log(data);
-      if (data.length > 10) {
-        return Notify.failure(
-          'Too many matches found. Please enter a more specific name.'
-        );
-      } else if (input.value.length === 0) {
-        return;
-      }
-      if (data.lenght >= 2 || data.length <= 10) {
-        for (const country of data) {
-          let arrLang = [];
-          for (lang in country.languages) {
-            arrLang.push(lang);
-          }
-          list.insertAdjacentHTML(
-            'afterbegin',
-            `<li>
-            <img src="${country.flags.svg}" width="20" height="20"> 
-              ${country.name.common} 
-            </li>`
-          );
-        }
-      }
-      if (data.length === 1) {
-        list.innerHTML = '';
-        for (const country of data) {
-          let arrLang = [];
-          for (lang in country.languages) {
-            arrLang.push(lang);
-          }
-          list.insertAdjacentHTML(
-            'afterbegin',
-            `<li>
-            <div style="display: flex;"><img src="${country.flags.svg}" width="60" height="60"> 
-              <h1>${country.name.common} </h1></div>
-             <div style="display:flex;"><h2>Capital: </h2><p class="data-details" style="font-size: 21px; font-weight: 500; padding-top: 2px; padding-left: 12px;">${country.capital}</p></div>
-              <div style="display:flex;"><h2>Population: </h2><p class="data-details" style="font-size: 21px; font-weight: 500; padding-top: 2px; padding-left: 12px;">${country.population} </p></div>
-              <div style="display:flex;"><h2>Languages: </h2><p class="data-details" style="font-size: 21px; font-weight: 500; padding-top: 2px; padding-left: 12px;">${arrLang}</p></div>
-            </li>`
-          );
-        }
-      }
-      if (data.status === 404) {
-        console.log(data);
-        return Notify.failure('Oops, there is no country with that name');
-      }
-    })
-    .catch(data => {
-      return Notify.failure('failed to load the resources');
-    });
+  return fetch(URL_COUNTRIES_API + name).then(response => response.json());
 }
+
 export { fetchCountries };
+
+// const input = document.querySelector('input[type=text]');
+// const list = document.querySelector('.country-list');
+// const DATABASE2 = 'https://restcountries.com/v3.1/name/';
+// import { Notify } from '../node_modules/notiflix/build/notiflix-notify-aio';
+
+// function fetchCountries(name) {
+//   name = input.value.trim();
+//   if (list.childNodes.length > 0) {
+//     list.innerHTML = '';
+//   }
+//   fetch(DATABASE2 + name)
+//     .then(response => {
+//       return response.json();
+//     })
+//     .then(data => {
+//       console.log(data.status);
+//       console.log(data);
+//       if (data.length > 10) {
+//         return Notify.failure(
+//           'Too many matches found. Please enter a more specific name.'
+//         );
+//       } else if (input.value.length === 0) {
+//         return;
+//       }
+//       if (data.lenght >= 2 || data.length <= 10) {
+//         for (const country of data) {
+//           let arrLang = [];
+//           for (lang in country.languages) {
+//             arrLang.push(lang);
+//           }
+//           list.insertAdjacentHTML(
+//             'afterbegin',
+//             `<li>
+//             <img src="${country.flags.svg}" width="20" height="20">
+//               ${country.name.common}
+//             </li>`
+//           );
+//         }
+//       }
+//       if (data.length === 1) {
+//         list.innerHTML = '';
+//         for (const country of data) {
+//           let arrLang = [];
+//           for (lang in country.languages) {
+//             arrLang.push(lang);
+//           }
+//           list.insertAdjacentHTML(
+//             'afterbegin',
+//             `<li>
+//             <div style="display: flex;"><img src="${country.flags.svg}" width="60" height="60">
+//               <h1>${country.name.common} </h1></div>
+//              <div style="display:flex;"><h2>Capital: </h2><p class="data-details" style="font-size: 21px; font-weight: 500; padding-top: 2px; padding-left: 12px;">${country.capital}</p></div>
+//               <div style="display:flex;"><h2>Population: </h2><p class="data-details" style="font-size: 21px; font-weight: 500; padding-top: 2px; padding-left: 12px;">${country.population} </p></div>
+//               <div style="display:flex;"><h2>Languages: </h2><p class="data-details" style="font-size: 21px; font-weight: 500; padding-top: 2px; padding-left: 12px;">${arrLang}</p></div>
+//             </li>`
+//           );
+//         }
+//       }
+//       if (data.status === 404) {
+//         console.log(data);
+//         return Notify.failure('Oops, there is no country with that name');
+//       }
+//     })
+//     .catch(data => {
+//       return Notify.failure('failed to load the resources');
+//     });
+// }
+// export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,16 +1,91 @@
-import { add, debounce } from 'lodash';
+import debounce from 'lodash';
+import { Notify } from 'notiflix';
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
-
-const input = document.querySelector('input[type=text]');
-const list = document.querySelector('.country-list');
-list.style.listStyle = 'none';
-
+const input = document.querySelector('input#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
 
-input.addEventListener(
-  'input',
-  debounce(() => {
-    fetchCountries();
-  }, DEBOUNCE_DELAY)
-);
+function singleMarkup(data) {
+  const langArr = [];
+  const lang = Object.values(data[0].languages);
+  console.log(lang);
+  for (let i = 0; i < lang.length; i++) {
+    langArr.push(lang[i]);
+  }
+  console.log(langArr);
+  return countryInfo.insertAdjacentHTML(
+    'afterbegin',
+    `<li style="list-style: none;">
+             <div style="display: flex;"><img src="${data[0].flags.svg}" width="60" height="60">
+               <h1>${data[0].name.common} </h1></div>
+             <div style="display:flex;"><h2>Capital: </h2><p class="data-details" style="font-size: 21px; font-weight: 500; padding-top: 2px; padding-left: 12px;">${data[0].capital}</p></div>
+               <div style="display:flex;"><h2>Population: </h2><p class="data-details" style="font-size: 21px; font-weight: 500; padding-top: 2px; padding-left: 12px;">${data[0].population} </p></div>
+           <div style="display:flex;"><h2>Languages: </h2><p class="data-details" style="font-size: 21px; font-weight: 500; padding-top: 2px; padding-left: 12px;">${langArr}</p></div>
+           </li>`
+  );
+}
+
+function multipleMarkup(data) {
+  for (const item of data) {
+    countryList.insertAdjacentHTML(
+      'afterbegin',
+      `<li>
+            <img src="${item.flags.svg}" width="20" height="20">
+              ${item.name.common}
+          </li>`
+    );
+  }
+}
+
+function clearMarkup() {
+  countryInfo.innerHTML = '';
+  countryList.innerHTML = '';
+}
+
+// function to search and make certain markups
+function fetch(e) {
+  const name = e.target.value;
+  clearMarkup();
+  if (name.length === 0) {
+    return Notify.failure('empty field!');
+  }
+
+  fetchCountries(name)
+    .then(data => {
+      if (data.length > 10) {
+        return Notify.failure(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        // adding single and multiple markups    vv
+      } else if (data.length >= 2 && data.length < 10) {
+        multipleMarkup(data);
+      }
+      if (data.length === 1) {
+        singleMarkup(data);
+      }
+    })
+    .catch(err => {
+      Notify.failure('failed to load resources');
+    });
+}
+
+input.addEventListener('input', debounce(fetch, DEBOUNCE_DELAY));
+
+// import { add, debounce } from 'lodash';
+// import './css/styles.css';
+// import { fetchCountries } from './fetchCountries';
+
+// const input = document.querySelector('input[type=text]');
+// const list = document.querySelector('.country-list');
+// list.style.listStyle = 'none';
+
+// const DEBOUNCE_DELAY = 300;
+
+// input.addEventListener(
+//   'input',
+//   debounce(() => {
+//     fetchCountries();
+//   }, DEBOUNCE_DELAY)
+// );
